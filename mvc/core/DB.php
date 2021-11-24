9580082a2a585528b96b9129edb69a90f8dfa6a6
To Hoa <tohoast09@gmail.com>
2021-11-25 00:33:53 +0700

    update DB.php

--- mvc/core/DB.php
+++ mvc/core/DB.php
@@ -15,4 +15,52 @@ class DB
         mysqli_select_db($this->con, $this->dbname);
         mysqli_query($this->con, "SET NAMES 'utf8'");
     }
+
+    // Select or Read data
+    public function select($query)
+    {
+        $result = $this->con->query($query) or
+            die($this->con->error . __LINE__);
+        if ($result->num_rows > 0) {
+            return $result;
+        } else {
+            return false;
+        }
+    }
+
+    // Insert data
+    public function insert($query)
+    {
+        $insert_row = $this->con->query($query) or
+            die($this->con->error . __LINE__);
+        if ($insert_row) {
+            return $insert_row;
+        } else {
+            return false;
+        }
+    }
+
+    // Update data
+    public function update($query)
+    {
+        $update_row = $this->con->query($query) or
+            die($this->con->error . __LINE__);
+        if ($update_row) {
+            return $update_row;
+        } else {
+            return false;
+        }
+    }
+
+    // Delete data
+    public function delete($query)
+    {
+        $delete_row = $this->con->query($query) or
+            die($this->con->error . __LINE__);
+        if ($delete_row) {
+            return $delete_row;
+        } else {
+            return false;
+        }
+    }
 }
