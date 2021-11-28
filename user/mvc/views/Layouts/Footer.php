1d44893cb1acd7b04016ad6e30408c069302cfa4
thanhdat <thanhdat.dev.js@gmail.com>
2021-11-28 13:59:07 +0700

    btn -scrolltop

--- user/mvc/views/Layouts/Footer.php
+++ user/mvc/views/Layouts/Footer.php
@@ -35,6 +35,12 @@
   </div>
 </div>
 
+<a href="#logo">
+  <div class="scrolltop">
+    <a href="#logo">^</a>
+  </div>
+</a>
+
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
   integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.min.js"
--- user/mvc/views/intro.php
+++ user/mvc/views/intro.php
@@ -4,7 +4,13 @@ require_once __DIR__ . "./Layouts/Header.php";
 ?>
 
 <div class="intro">
-    <h3>Phan INTRO LAM SAU, HTML TINH</h3>
+  <div class="top">
+    <h3>Chúng tôi là ai</h3>
+    <p>Đơn vị cung ứng giải pháp dữ liệu, ứng dụng công nghệ IoT phục vụ quá trình chuyển đổi số cho doanh nghiệp.</p>
+  </div>
+  <div class="mid">
+
+  </div>
 </div>
 <?php
 require_once __DIR__ . './Layouts/Footer.php';
