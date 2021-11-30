4e57d17d8f9ce82b7fa8e7ff66ef59260bca9c49
tvpluss <truongphuocst310301@gmail.com>
2021-11-30 12:37:32 +0700

    fix paging view

--- admin/mvc/views/ShowProduct.php
+++ admin/mvc/views/ShowProduct.php
@@ -53,11 +53,13 @@ require_once __DIR__ . "./Layouts/Header.php";
   </table>
   <nav aria-label="Page navigation example">
     <ul class="pagination">
-      <li class="page-item"><a class="page-link" href="./Product?page=1">First</a></li>
-      <li class="page-item"><a class="page-link" href="./Product?page=<?php echo ($data['currentPage'] - 1) ?>">Previous</a></li>
+      <li class="page-item"><a class="page-link" href="./Product?page=1">Trang đầu</a></li>
+      <li class="page-item"><a class="page-link" href="./Product?page=<?php echo ($data['currentPage'] - 1) ?>">
+          << </a>
+      </li>
       <li class="page-item"><a class="page-link" href="#"><?php echo ($data['currentPage']) ?></a></li>
-      <li class="page-item"><a class="page-link" href="./Product?page=<?php echo ($data['currentPage'] + 1) ?>">Next</a></li>
-      <li class="page-item"><a class="page-link" href="./Product?page=<?php echo ($data['totalPages']) ?>">Last</a></li>
+      <li class="page-item"><a class="page-link" href="./Product?page=<?php echo ($data['currentPage'] + 1) ?>"> >> </a></li>
+      <li class="page-item"><a class="page-link" href="./Product?page=<?php echo ($data['totalPages']) ?>">Trang cuối</a></li>
     </ul>
   </nav>
 </div>
